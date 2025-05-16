9a13175894c928528f809fe873f32f497cd54cfd
eljamm <fedi.jamoussi@protonmail.ch>
2025-05-17 00:33:10 +0200

    Split preparation and cleanup

--- helpers/prepare.sh
+++ helpers/prepare.sh
@@ -2,38 +2,6 @@
 
 case "$RUNNER_OS" in
 Linux)
-  if [ "$CLEAN" = true ]; then
-    echo "Disk clean, before:"
-    df -h -x tmpfs
-    sudo rm -rf \
-      /etc/skel/.cargo \
-      /etc/skel/.dotnet \
-      /etc/skel/.rustup \
-      /home/runner/.cargo \
-      /home/runner/.dotnet \
-      /home/runner/.rustup \
-      /home/runneradmin/.cargo \
-      /home/runneradmin/.dotnet \
-      /home/runneradmin/.rustup \
-      /opt/az \
-      /opt/google \
-      /opt/hostedtoolcache \
-      /opt/microsoft \
-      /opt/pipx \
-      /root/.sbt \
-      /usr/lib/google-cloud-sdk \
-      /usr/lib/jvm \
-      /usr/local \
-      /usr/share/az_* \
-      /usr/share/dotnet \
-      /usr/share/miniconda \
-      /usr/share/swift
-    docker image prune --all --force >/dev/null
-    echo
-    echo "After:"
-    df -h -x tmpfs
-    echo
-  fi
   if [ "$BTRFS" = true ]; then
     echo "Make /nix BTRFS RAID0 from /btrfs and /mnt/btrfs"
     sudo touch /btrfs /mnt/btrfs
@@ -54,23 +22,6 @@ Linux)
   fi
   ;;
 macOS)
-  if [ "$CLEAN" = true ]; then
-    echo "Disk clean, before:"
-    df -h /
-    sudo rm -rf \
-      /Applications/Xcode_* \
-      /Library/Developer/CoreSimulator \
-      /Library/Frameworks \
-      /Users/runner/.dotnet \
-      /Users/runner/.rustup \
-      /Users/runner/Library/Android \
-      /Users/runner/Library/Caches \
-      /Users/runner/Library/Developer/CoreSimulator \
-      /Users/runner/hostedtoolcache
-    echo
-    echo "After:"
-    df -h /
-  fi
   # This save about 110G disk space, and take about 0.6s
   sudo rm -rf \
     /Library/Developer/CoreSimulator \
